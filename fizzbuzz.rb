5e8bf5def878d8f93fca323e773cecd8a42ec71b
abiazahir <abiazahir@hotmail.com>
2018-06-19 19:45:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(int)
   if int%15 ==0
-    puts "Fizz"
+    puts "FizzBuzz"
   elsif int%5==0
     puts "Buzz"
   elsif int%3==0
-   puts "FizzBuzz"
+   puts "Fizz"
   else
     puts "nil" 
   end 
